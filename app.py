7809be9e4562fa0b4c6b2dd4dc83bfcd2ad8a1fe
octagm <octagm3001@gmail,com>
2025-05-18 17:26:18 -0500

    Agregado Nueva Categoria ENFERMEDA TERMINAL

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # <--- Importa CORS
 
+
 app = Flask(__name__)
 CORS(app)  # <--- Habilita CORS en toda la app
 
 def diagnostico(temperatura, ritmo_cardiaco, saturacion_oxigeno):
     if 36 <= temperatura < 37.5 and 60 <= ritmo_cardiaco <= 100 and saturacion_oxigeno >= 95:
         return "NO ENFERMO"
-    elif 37.5 <= temperatura < 38.5 and 100 < ritmo_cardiaco <= 120 and saturacion_oxigeno >= 92:
+    elif 37.5 <= temperatura < 38.5 and 100 < ritmo_cardiaco <= 120 and 92 <= saturacion_oxigeno < 95:
         return "ENFERMO LEVE"
-    elif temperatura >= 38.5 and ritmo_cardiaco > 120 and saturacion_oxigeno < 92:
+    elif 38.5 <= temperatura < 40 and ritmo_cardiaco > 120 and 90 <= saturacion_oxigeno < 92:
         return "ENFERMEDAD AGUDA"
-    elif 37 <= temperatura < 38 and 60 <= ritmo_cardiaco < 100 and saturacion_oxigeno < 90:
+    elif temperatura >= 40 and ritmo_cardiaco > 120 and 85 <= saturacion_oxigeno < 90:
         return "ENFERMEDAD CRÓNICA"
+    elif temperatura >= 40  and ritmo_cardiaco > 120 and saturacion_oxigeno < 85:
+        return "ENFERMEDAD TERMINAL"
     else:
-        return "Valores fuera de rango, consulte a un médico."
+        return "Valores fuera de rango. Por favor, consulte a un médico."
 
 @app.route('/diagnostico', methods=['POST'])
 def diagnosticar():
